b17ff4f02939a667835e4f452df449d4495d425d
owenmadd3 <owenmadd3@gmail.com>
2026-06-04 09:05:42 -0500

    a
    k

--- data/new_highs_lows.py
+++ data/new_highs_lows.py
@@ -501,7 +501,7 @@ fig.update_xaxes(showgrid=True, gridcolor="#161616", tickfont=dict(color="#666",
 fig.update_xaxes(showgrid=True, gridcolor="#161616", tickfont=dict(color="#666", size=10, family="monospace"),
                  rangeslider_visible=False, **spike, row=2, col=1)
 
-st.plotly_chart(fig, width="stretch", config={
+st.plotly_chart(fig, use_container_width=True, config={
     "scrollZoom": True, "displayModeBar": True, "displaylogo": False,
 })
 
@@ -571,4 +571,4 @@ with st.expander("Raw Breadth Data"):
     display = breadth[["new_highs","new_lows"]].copy()
     display.columns = [f"{BBG_HI[exchange]} (New Highs)", f"{BBG_LO[exchange]} (New Lows)"]
     display.index = display.index.strftime("%m/%d/%Y")
-    st.dataframe(display.iloc[::-1].head(120), width="stretch", height=320)
+    st.dataframe(display.iloc[::-1].head(120), use_container_width=True, height=320)
